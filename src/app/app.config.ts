36bd31ae32c8d9b286172d3635a4c8eb719b3293
Martin Barroso <79174533+tinchomdb@users.noreply.github.com>
2026-02-27 20:09:30 +0100

    refactor: remove unused import for provideAnimationsAsync

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
